9bad630782539ed54b421dc766d541a046fc2ff2
nivek <literalmentebruno@gmail.com>
2024-11-29 16:13:29 -0300

    scripty-target-adicionado

--- scripty.js
+++ scripty.js
@@ -1,1 +1,32 @@
-const columns = document.querySelectorAll('column__cards');
+const columns = document.querySelectorAll('.column__cards');
+const cards = document.querySelectorAll('.card');
+let draggedCard;
+
+const dragStart = (event) => {
+    draggedCard = event.target;
+    event.dataTransfer.effectAllowed= 'move';
+};
+const dragOver = (event) => {
+    event.preventDefault();
+
+};
+const dragEnter = ({target}) => {
+    if (target.classList.contains('column__cards')){
+        target.classList.add('column--highlight');
+    }
+};
+const dragLeave = ({target}) => {
+     
+    };
+
+
+
+cards.forEach((card) => {
+    card.addEventListener('dragstart', dragStart);
+});
+
+columns.forEach((column) => {
+    column.addEventListener('dragover', dragOver);
+    column.addEventListener('dragenter', dragEnter);
+    column.addEventListener('dragleave', dragEnter);
+});
